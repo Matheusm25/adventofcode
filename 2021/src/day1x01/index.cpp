0ee1432c2130d8ebc92908789e03ff389d6e0a4b
matheusm25 <matheusml25@hotmail.com>
2022-12-04 18:19:21 -0300

    2021: day 1x01

--- 2021/src/day1x01/index.cpp
+++ 2021/src/day1x01/index.cpp
@@ -1,7 +1,26 @@
 using namespace std;
+
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
-    cout << "Hello, World!" << endl;
+    ifstream testInput ("./sample-input.txt");
+    ifstream realInput ("./input.txt");
+    string input;
+
+    int increments = 0;
+    getline(realInput, input);
+    int lastLine = stoi(input);
+
+    while (getline(realInput, input)) {
+      int line = stoi(input);
+      if (line > lastLine) {
+        increments++;
+      }
+      lastLine = line;
+    }
+
+    cout << "Challenge result: " << increments << endl;
     return 0;
 }
