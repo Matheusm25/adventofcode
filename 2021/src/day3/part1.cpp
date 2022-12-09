d8e40e3a3390a09412a2be585009d90d4adfc8d3
matheusm25 <matheusml25@hotmail.com>
2022-12-08 21:51:27 -0300

    2021: day 3 on going

---
+++ 2021/src/day3/part1.cpp
@@ -0,0 +1,57 @@
+using namespace std;
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    ifstream testInput ("./sample-input.txt");
+    ifstream realInput ("./input.txt");
+    string input;
+
+    int result;
+    int fileLineCount = 0;
+
+    while (getline(realInput, input)) {
+      fileLineCount++;
+    }
+  
+    realInput.clear();
+    realInput.seekg(0, ios::beg);
+
+    string bitCode[fileLineCount]; 
+
+    for (int i = 0; i < fileLineCount; i++) {
+      getline(realInput, input);
+      bitCode[i] = input;
+    }
+
+    string gammaRate;
+    string epsilonRate;
+
+    for (int i = 0; i < bitCode[0].length(); i++) {
+      int trueCount = 0;
+      for (int j = 0; j < fileLineCount; j++) {
+        if (bitCode[j][i] == '1') {
+          trueCount++;
+        }
+      }
+
+      if (trueCount > fileLineCount / 2) {
+        gammaRate += '1';
+        epsilonRate += '0';
+      } else {
+        gammaRate += '0';
+        epsilonRate += '1';
+      }
+    }
+
+    result = stoi(gammaRate, 0, 2) * stoi(epsilonRate, 0, 2);
+
+    cout << "Gamma rate: " << gammaRate << endl;
+    cout << "Epsilon rate: " << epsilonRate << endl;
+
+
+    cout << "Challenge result: " << result << endl;
+    return 0;
+}
